f509a9af7d0ba2160a0e8aef869a02b550f7b771
Jharna01 <menokasaharia@gmail.com>
2024-10-24 19:17:18 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import WeatherGet from './WeartherGet/Weatherget'
+
+
+const App = () => {
+  return (
+    <div>
+      <WeatherGet/>
+    </div>
+  )
+}
+
+export default App
